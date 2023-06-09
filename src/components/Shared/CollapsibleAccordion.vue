2878d5e79b587d030b10e77204b3828224c8ebbe
yasith <wimukthiyasith@gmail.com>
2023-06-09 23:41:40 +0530

    implement action button

---
+++ src/components/Shared/CollapsibleAccordion.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="border-b border-solid border-brand-gray-2 py-5">
+    <div
+      class="flex cursor-pointer flex-wrap items-center justify-between"
+      @click="open"
+    >
+      <h3 class="text-base font-semibold">{{ header }}</h3>
+      <font-awesome-icon :icon="carretIcon" />
+    </div>
+    <div v-if="isOpen" class="mt-5 w-full">
+      <slot>
+        <p>Whoops, somebody forgot to populate me!</p>
+      </slot>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { ref, computed } from "vue";
+defineProps({
+  header: {
+    type: String,
+    required: true,
+  },
+});
+
+const isOpen = ref(false);
+
+const carretIcon = computed(() =>
+  isOpen.value ? ["fas", "angle-up"] : ["fas", "angle-down"]
+);
+
+const open = () => {
+  isOpen.value = !isOpen.value;
+};
+</script>
