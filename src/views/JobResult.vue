54995f0e1b7d4659f55db73aa117ae5b8ff61986
yasith <wimukthiyasith@gmail.com>
2023-06-18 21:26:56 +0530

    implement Hero section

---
+++ src/views/JobResult.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="flex w-full flex-row flex-nowrap">
+    <JobFilterSidebarVue />
+    <JobListingVue />
+  </div>
+</template>
+
+<script lang="ts" setup>
+import JobFilterSidebarVue from "../components/JobResult/JobFilterSidebar/JobFilterSidebar.vue";
+import JobListingVue from "../components/JobResult/JobListings.vue";
+</script>
