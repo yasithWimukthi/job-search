8dad9b2f496059545393856b5281567156a161d8
yasith <wimukthiyasith@gmail.com>
2023-06-26 23:32:55 +0530

    implement Hero section

---
+++ src/views/TeamsView.vue
@@ -0,0 +1,16 @@
+<template>
+  <HeaderContainer>
+    <template #title>
+      <h1 class="w-full text-4xl font-normal">Teams</h1>
+    </template>
+    <template #subtitle>
+      <h2 class="my-4 w-full text-base">
+        It's awesome working here. Why don't you come join us?
+      </h2>
+    </template>
+  </HeaderContainer>
+</template>
+
+<script lang="ts" setup>
+import HeaderContainer from "../components/Shared/HeaderContainer.vue";
+</script>
